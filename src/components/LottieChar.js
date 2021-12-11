36689e3288c9ff3eada8c39a9654b894ae34dd0d
mahan-sepehri <mahan.sepehrifar@gmail.com>
2021-12-12 01:14:07 +0330

    changed images to buttons

--- src/components/LottieChar.js
+++ src/components/LottieChar.js
@@ -1,22 +1,13 @@
 import React, { useEffect, useRef } from "react";
 import Lottie from "lottie-web";
 
-import backButton from "../assets/left-arrow.svg";
-import forwardButton from "../assets/right-arrow.svg";
+// import backButton from "../assets/left-arrow.svg";
+// import forwardButton from "../assets/right-arrow.svg";
 import "./LottieChar.css";
 
 let walkingOrange;
 
 const LottieChar = () => {
-  // var animation = bodymovin.loadAnimation({
-  //     container: document.getElementById('lottie'), // Required
-  //     path: 'data.json', // Required
-  //     renderer: 'svg/canvas/html', // Required
-  //     loop: true, // Optional
-  //     autoplay: true, // Optional
-  //     name: "Hello World", // Name for future reference. Optional.
-  //   })
-
   const lottieContainerRef = useRef();
 
   const forwardWalkHandler = () => {
@@ -69,32 +60,16 @@ const LottieChar = () => {
         <div className="lottie-container" ref={lottieContainerRef}></div>
       </div>
       <div className="button-container">
-        <img
-          src={backButton}
-          alt="backward"
-          className="back-button"
+        <button
           onMouseDown={backwardWalkHandler}
           onMouseUp={() => walkingOrange.stop()}
-        />
-        {/* <button
-          onMouseDown={backwardWalkHandler}
-          onMouseUp={() => walkingOrange.stop()}
-        >
-          backward
-        </button> */}
-        {/* <button
-          onMouseDown={forwardWalkHandler}
-          onMouseUp={() => walkingOrange.stop()}
-        >
-          forward
-        </button> */}
-        <img
-          src={forwardButton}
-          alt="backward"
           className="back-button"
+        ></button>
+        <button
           onMouseDown={forwardWalkHandler}
           onMouseUp={() => walkingOrange.stop()}
-        />
+          className="forward-button"
+        ></button>
       </div>
     </div>
   );
